e214e4de52ef6e4c2fb034581498f62867e9d187
Lucas robson <lucas.filhotar@gmail.com>
2025-08-01 17:00:31 -0300

    Add files via upload

---
+++ lista.java
@@ -0,0 +1,86 @@
+
+public class lista{
+  public static void main(String args[]) {
+//1
+double I = 300.00;
+double L = 150.00;
+double M = 499.99;
+double total = I+M+L;
+
+  System.out.println("o cliente deverá pagar para a empresa "+ total);
+
+//2
+int j = 35;
+ int o = 8;
+ int t = j*o;
+
+System.out.println("ao final de um dia ele deve receber "+ t);
+
+//3
+int j1 = 27;
+ int o1 = 16;
+ int t1 = j1*o1;
+
+System.out.println("ela tem "+ t1 + " bombons");
+
+//4
+int g = 300;
+ int a1 = 20;
+ int total1 = g / a1;
+
+System.out.println("ela tem "+ total1 + " garrafas");
+
+//5
+
+ int camisetasCompradas1 = 120;
+        int camisetasVendidas = 80;
+        double precoCamiseta = 30.0;
+
+        int camisetasRestantes = camisetasCompradas1 - camisetasVendidas;
+
+        int camisetasCompradas2 = 50;
+
+        int camisetasTotais = camisetasRestantes + camisetasCompradas2;
+
+        double faturamento = camisetasVendidas * precoCamiseta;
+
+        System.out.println("Camisetas em estoque agora: " + camisetasTotais);
+        System.out.println("Faturamento total: R$ " + faturamento);
+
+
+//6
+ double numero = 12.0; // Você pode trocar esse número por qualquer outro
+
+        double dobro = numero * 2;
+        double metade = numero / 2;
+
+        System.out.println("Número original: " + numero);
+        System.out.println("Dobro do número: " + dobro);
+        System.out.println("Metade do número: " + metade);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}

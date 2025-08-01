e3defa0adba02deeee3808362c6da63f81574e2c
Lucas robson <lucas.filhotar@gmail.com>
2025-08-01 16:20:24 -0300

    meu primeiro projeto em java

---
+++ MyClass.java
@@ -0,0 +1,50 @@
+
+public class MyClass {
+  public static void main(String args[]) {
+   System.out.println("ola mundo");
+  
+
+int idade = 16; // numeros inteiros
+double salario =1.78;// numeros quebrados
+char letra = 'j'; //letra ou simbolo
+boolean estudante = true; //verdadeiro e falso
+String nome = "lucas robson" ;// texto
+  System.out.print(idade);
+     System.out.print("minha idade é " + idade);
+     System.out.print("meu salario é " + idade);  
+     System.out.print("a letra é " + idade);
+     System.out.print("sou estudante" + estudante);
+       System.out.println("meu nome é" + nome);
+       int n1 =5;
+       int n2=2;
+       int sub = n1- n2;
+       int soma = n2 +n1;
+       int mult = n1*n2;
+   double div = n1 / n2;
+     System.out.print(soma);
+     System.out.print(sub);
+     System.out.print(div);
+     System.out.println(mult);
+   
+    double largura = 7.5;
+     double comprimento = 3.0;
+    double area = largura * comprimento;
+   System.out.println("area"+area);
+   
+    double valor = 100;
+     double desconto = 15;
+    double vd = valor * (desconto /100);
+      double vf = valor - vd;
+
+      System.out.println ("a pagar" + vf);
+  
+   
+   
+   
+   
+   
+   
+   
+   
+  }
+}

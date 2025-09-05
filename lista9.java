c7901587942f54038a6dc12ef52382cb824926a3
Lucas robson <lucas.filhotar@gmail.com>
2025-09-05 16:22:05 -0300

    Add files via upload

---
+++ lista9.java
@@ -0,0 +1,31 @@
+package primeiroprojeto;
+
+public class lista9 {
+
+	public static void main(String[] args) {
+		// 1 
+		   int numero = 1;
+	        while (numero <= 10) {
+	            System.out.println(numero);
+	            numero++; // +1
+	        }
+  //2
+	        int numero2 = 10;
+	        while (numero2 >= 1) {
+	            System.out.println(numero2);
+	            numero2--; // -1
+	        }
+
+	     // 3
+	        for (int nm = 0; nm <= 100; nm += 5) {
+	            System.out.println(nm);
+	        }
+
+	        // 4 
+	        for (int i = 0; i < 5; i++) {
+	            System.out.println("Eu gosto de Java");
+	        }
+
+	      
+	}
+}
